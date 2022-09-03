6610c33bedfc6eb0bdd3f585702a3ee4ae469077
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2022-09-03 19:35:55 +0600

    id fetched and total item plus catagory section completed

--- js/app.js
+++ js/app.js
@@ -2,29 +2,46 @@ const loadNavbar = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
         .then(res => res.json())
-        .then(data => showNavbar(data.data.news_category));
+        .then(data => showNavbar(data.data.news_category))
+        .catch(error => console.log(error));
 }
 let arr = [];
 const showNavbar = array => {
     const navBar = document.getElementById('navbar-nav');
-    navBar.textContent="";
-    navBar.innerHTML =
-        `
-        <a class="nav-link fs-5 mx-3" aria-current="page" href="#">Home</a>
-        `;
+    navBar.textContent = "";
+    
     arr = array;
-    // console.log(arr);
     array.forEach(ar => {
-        console.log(ar);
-        const anchorDiv=document.createElement('div');
+        // console.log(ar);
+        const anchorDiv = document.createElement('div');
 
-            anchorDiv.innerHTML =
+        anchorDiv.innerHTML =
             `
-            <a class="nav-link fs-5 mx-3" aria-current="page" href="#">${ar.category_name}</a>
+            <a class="nav-link fs-5 mx-3" aria-current="page" href="#"  onclick="cardshow('${ar.category_id}' ,'${ar.category_name}')">${ar.category_name}</a>
             `;
-            navBar.appendChild(anchorDiv);
+        navBar.appendChild(anchorDiv);
     }
     )
+}
+
+const cardshow =(id ,name)=> {
+    // console.log('id:',id,'name:',name);
+    const catagoryName=document.getElementById('catagory-name');
+    catagoryName.innerText=name;
+    const url2 = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => displayCard(data.data,data.data.length))
+        .catch(error => console.log(error));
+
+}
+
+const displayCard=(info,count)=>{
+    const totalCard=document.getElementById('total-card');
+    totalCard.innerText=count;
+    console.log(info);
+    
 }
 
 loadNavbar();
